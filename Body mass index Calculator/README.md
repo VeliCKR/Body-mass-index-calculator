e8b969b965c51148ebc48e2215b993501221423c
VeliCKR <velicr57@gmail.com>
2022-11-21 18:37:38 +0300

    Body mass index calculator
    This application is body mass index calculator.

---
+++ Body mass index Calculator/README.md
@@ -0,0 +1,9 @@
+
+
+## Body Mass index
+
+This application is body mass index calculator.
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ Body mass index Calculator/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bmi/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Body mass index calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
